4e7fa8faea9ed47e287dea281d47b31f78fa8b1e
PR <rehmann_peter@web.de>
2021-09-06 20:18:28 +0200

    addTask function added

--- js/addTask.js
+++ js/addTask.js
@@ -1,3 +1,4 @@
+let allTasks = [];
 
 
 function changeUrgency(){
@@ -20,4 +21,35 @@ function mediumUrgency(){
 function lowUrgency(){
     document.getElementById('urgency-status-field').innerHTML = 'Low';
     document.getElementById('urgency-content').classList.add('d-none');
+}
+
+function addTask() {
+    let title = document.getElementById('title-field').value;
+    let description = document.getElementById('description-field').value;
+    let date = document.getElementById('date-field').value;
+
+    // document.getElementById('todo-container').innerHTML = '';
+    let task = {
+        'title': title,
+        'description': description,
+        // 'Category': Category,
+        'date': date
+    };
+
+    allTasks.push(task);
+
+    for (let i = 0; i < allTasks.length; i++) {
+        document.getElementById('todo-container').innerHTML += `
+        <div class="task-unit">
+            <div class="task-container">
+                <span id="task-title">${allTasks[i].title}</span>
+                <span id="description-container"></span>
+                <span id="category-container" class="extra-info" >sfs</span>
+                <span id="date-container" class="extra-info" >sdf</span>
+                <span id="urgency-container" class="extra-info" >sdf</span>
+                <span id="assigned-container" class="extra-info" >sdf</span>
+            </div>
+        </div
+        `;
+    }
 }
