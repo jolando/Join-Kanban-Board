7be0b18ed77238fda4cad4bb237b9d8b0c7e807e
SebSeitz <sebastian_seitz13@web.de>
2021-09-12 11:13:32 +0200

    profile-adjustment

--- js/addTask.js
+++ js/addTask.js
@@ -33,8 +33,10 @@ function renderUserProfiles(){
     document.getElementById('assignment-box').innerHTML += `
     <div class="profile-box" id="profile">
     <img src="img/profile.png" class="task-profile-pic" />
-    <span>${profiles[i]['name']}</span>
+    <div class="span-container" id="span-container${i}">
+    <span>${profiles[i]['name']},&#160</span>
     <span>${profiles[i]['e-mail']}</span>
+    </div>
     <img src="img/icon plus.png" class="plus-icon" onclick="selectUser(${i})"/>
     <span id="name-container"></span>
 </div>
@@ -49,15 +51,16 @@ function selectUser(index){
     "name": `${profiles[index].name}`,
     "e-mail": `${profiles[index]['e-mail']}`
   }
+  document.getElementById(`span-container${index}`).classList.add('selected-user-box');
+  document.getElementById(`span-container${index}`).style.color = "white";
   userCache.push(user);
   console.log(user);
   saveCreatedProfiles();
   getProfileInfo();
-
  
 }
 
-function addTask(event) {
+function addTask(event,) {
   event.preventDefault();
   let title = document.getElementById("title-field").value;
   let description = document.getElementById("description-field").value;
@@ -84,28 +87,8 @@ function addTask(event) {
   localStorage.setItem("allTasks", allTasksAsString);
 
   document.forms['addTask-form'].reset();
-  document.getElementById('name-selection').classList.add('d-none');
-  document.getElementById('profile').style.justifyContent = "flex-start";
+
+  //document.getElementById(`span-container${index}`).style.backgroundColor ="none";
+  //document.getElementById(`span-container${index}`).style.color ="black";
 }
 
-// function changeUrgency(){
-//     let urgency = document.getElementById('urgency-content');
-//     if (urgency.classList.contains('d-none')){
-//         urgency.classList.remove('d-none')
-//     } else {
-//         urgency.classList.add('d-none');
-//     }
-// }
-
-// function highUrgency(){
-//     document.getElementById('urgency-status-field').innerHTML = 'High';
-//     document.getElementById('urgency-content').classList.add('d-none');
-// }
-// function mediumUrgency(){
-//     document.getElementById('urgency-status-field').innerHTML = 'Medium';
-//     document.getElementById('urgency-content').classList.add('d-none');
-// }
-// function lowUrgency(){
-//     document.getElementById('urgency-status-field').innerHTML = 'Low';
-//     document.getElementById('urgency-content').classList.add('d-none');
-// }
