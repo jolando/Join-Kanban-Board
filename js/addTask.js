6678134d7bd42306e95ee9022168336044af4762
SebSeitz <sebastian_seitz13@web.de>
2023-03-30 21:05:54 +0200

    fixed deleted function in selectedUsers box with find method

--- js/addTask.js
+++ js/addTask.js
@@ -1,22 +1,13 @@
-let selectedUsers = [
-
-];
-let uniqueUserIDs;
+let selectedUsers = [];
 let newTask;
-
 
 async function initAddTask() {
   includeHTML();
   await getTasks();
   await getUsers();
   await renderUserProfiles();
-  // await loadRegisterRequest();
 }
 
-/**
- * This function saves the values of the form input fields into variables
- * @returns the input values of every form input field
- */
 function getInputValues() {
   newTask = {
     id: `${new Date().getTime()}`,
@@ -27,100 +18,78 @@ function getInputValues() {
     category: document.getElementById('task-category').value,
     status: 'todo',
     users: selectedUsers
-
   };
   return newTask;
 }
 
-/**
- * This function pushes the input values of the form into a global array and informs the user that a task has been created
- * @param {*} event -this is an onsubmit default event
- */
 function addTask(event) {
   event.preventDefault();
-  // if (selectedUser.length == 0) {
-  //   alert('You have to choose at least one user');
-  // } else {
-    executeAddTask();
-  // }
+  executeAddTask();
 }
 
-/**
- * This function creates a task and clears the input fields
- *
- */
 function executeAddTask() {
   let newTask = getInputValues();
-  // task.selectedId = selectedUser.map((u) => u.id);
-  // allTasks.push(task);
-  // saveAllTasks();
-  // tasks.push(task);
   saveTasks(newTask);
   showAddSymbol();
   alert('Task has been created');
   resetAddTask();
 }
 
-/**
- * This function displays the users who are saved in the backend
- */
 async function renderUserProfiles() {
   document.getElementById('assignment-box').innerHTML = '';
   for (let i = 0; i < newUsers.length; i++) {
+    let plusIcon = '';
+
+    if (!selectedUsers.includes(newUsers[i].id)) {
+      plusIcon = `<img id="plus${i}" src="img/icon-plus.png" class="plus-icon" onmouseover="addHighlight(${i})" onclick="pushSelectedUser(${i})"/>`;
+    }
 
     document.getElementById('assignment-box').innerHTML += `
-    <div class="profile-box" id="profile${i}">
-        <span>${newUsers[i].first_name}</span>
-        <span>${newUsers[i].last_name}</span>
-        <img id="plus${i}" src="img/icon-plus.png" class="plus-icon" onmouseover="addHighlight(${i})" onclick="pushSelectedUser(${i});"/>
-    </div>
+      <div class="profile-box" id="profile${i}">
+          <span>${newUsers[i].first_name}</span>
+          <span>${newUsers[i].last_name}</span>
+          ${plusIcon}
+      </div>
     `;
+  }
+
+  renderSelectedUsers();
+}
+
+function renderSelectedUsers() {
+  document.getElementById('assigned-to').innerHTML = '';
+
+  for (let i = 0; i < selectedUsers.length; i++) {
+    let user = newUsers.find(u => u.id === selectedUsers[i]);
+
+    if (user) {
+      document.getElementById('assigned-to').innerHTML += `
+        <div class="assigned-to-card">
+          <span>${user.first_name}</span>
+          <span>${user.last_name}</span>
+          <img onclick="removeSelectedUser(${i})" class="minus-icon" src="img/minus.png">
+        </div>
+      `;
+    }
   }
 }
 
-/**
- * This function pushes the id of a registeredUser into the "selectedUser" array
- */
 function pushSelectedUser(id) {
   let userID = newUsers[id].id;
-  selectedUsers.push(userID);
-  // filterUserArray();
-  document.getElementById('plus' + id).style = 'visibility: hidden;';
-  renderSelectedUser(id);
+
+  if (!selectedUsers.includes(userID)) {
+    selectedUsers.push(userID);
+    document.getElementById(`plus${id}`).style.visibility = 'hidden';
+    renderSelectedUsers();
+  }
 }
 
-// function filterUserArray(){
-//   uniqueUserIDs = selectedUsers.filter((item, i, ar) => ar.indexOf(item) === i);
-// }
-
-//1. Get users List users
-
-/**
- * This function adds the selected user to a separate box in order to highlight them
- */
-function renderSelectedUser(id) {
-  // document.getElementById('assigned-to').innerHTML = '';
-    document.getElementById('assigned-to').innerHTML += `
-    <div id="selected${id}" class="assigned-to-card">
-      <span>${newUsers[id].first_name}</span>
-      <span>${newUsers[id].last_name}</span>
-      <img onclick="removeSelectedUser(${id})" class="minus-icon" src="img/minus.png">
-    <div>
-   `;
-
-}
-
-/**
- *Deletes the selected users
- *
- * @param {index} index of allRegisteredUsers
- */
 function removeSelectedUser(index) {
   selectedUsers.splice(index, 1);
-  console.log(selectedUsers);
-  renderSelectedUser();
+  renderSelectedUsers();
   renderUserProfiles();
 }
+
 
 /**
  * Shows the pulse icons again after a task has been created
